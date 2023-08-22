2ff53e864b0da20f7feffcc4f53d2bbe0679ecd7
Artur Myronchuk <122921226+ArturMyronchuk@users.noreply.github.com>
2023-08-22 15:35:07 +0300

    44

--- src/components/Feedback/Statistics.jsx
+++ src/components/Feedback/Statistics.jsx
@@ -8,7 +8,6 @@ const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
 
   return (
     <div>
-      <h2>Feedback Statistics</h2>
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
